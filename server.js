73ea202fa22c41027bc7543c2c227f0acc05bd0a
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 05:08:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,5 +55,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
-const { startPriceUpdater } = require('./jobs/priceUpdater');
-startPriceUpdater();
