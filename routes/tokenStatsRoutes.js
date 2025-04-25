61c292cd270e32d7a31cdfb07de1ec3551c9aac5
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 18:00:57 +0530

    Delete routes directory

--- routes/tokenStatsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchTokenStats } = require('../controllers/tokenStatsController');
-
-router.get('/:contract', fetchTokenStats); // /api/token-stats/:contract?network=bsc
-
-module.exports = router;
