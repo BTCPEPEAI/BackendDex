b7ff26039bcf58e6d2c701fff06b224ca14aaf9c
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 23:12:21 +0530

    Update candleRoutes.js

--- routes/candleRoutes.js
+++ routes/candleRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Candle = require('../models/Candle');
+const { getCandlesByToken } = require('../controllers/candleController');
+
+router.get('/:address', getCandlesByToken);
 
 router.get('/:pairAddress', async (req, res) => {
   const candles = await Candle.find({ pairAddress: req.params.pairAddress })
