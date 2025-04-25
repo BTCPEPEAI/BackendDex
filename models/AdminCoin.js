3164e96d650c6faec86976daa7c1bb21e547fb6b
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 18:00:37 +0530

    Delete models directory

--- models/AdminCoin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminCoinSchema = new mongoose.Schema({
-  category: { type: String, required: true },
-  coins: [String], // contract addresses
-});
-
-module.exports = mongoose.model('AdminCoin', adminCoinSchema);
--- models/Watchlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const WatchlistSchema = new mongoose.Schema({
-  wallet: { type: String, required: true, unique: true },
-  tokens: [{ type: String }] // List of token addresses or symbols
-})
-
-module.exports = mongoose.model('Watchlist', WatchlistSchema)
