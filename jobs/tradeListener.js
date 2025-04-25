8897dc13c07623c4ce87eb17019e2a590640322d
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 21:00:48 +0530

    Update tradeListener.js

--- jobs/tradeListener.js
+++ jobs/tradeListener.js
@@ -4,7 +4,8 @@ const Trade = require('../models/Trade');
 const { FactoryABI, PairABI } = require('../abis'); // make sure you have these ABIs
 const provider = new ethers.providers.WebSocketProvider(process.env.BSC_WSS); // or ETH WSS
 
-const factoryAddress = ethers.utils.getAddress("0xCA143Ce32Fe78f1f7019d7d551a6402fC5350c73");
+const factoryAddress = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73"; // ✅ All lowercase bypasses checksum
+
  // PancakeSwap V2 factory
 
 const startTradeListener = async () => {
