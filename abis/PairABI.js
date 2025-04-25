150a487b9375feb45d70bd93aebf43eb26564b13
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-25 19:34:17 +0530

    Update PairABI.js

--- abis/PairABI.js
+++ abis/PairABI.js
@@ -14,3 +14,8 @@ module.exports = [
     }
   ];
   
+  module.exports = [
+    "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)",
+    "function getReserves() view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"
+  ];
+  
