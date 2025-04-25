095f3abab23b727ab0db033902f747fdb9be0eb7
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-26 04:58:48 +0530

    Update index.js

--- jobs/index.js
+++ jobs/index.js
@@ -3,3 +3,9 @@ const { generateCandles } = require('./candleGenerator');
 // Run every 1 minute
 setInterval(generateCandles, 60 * 1000);
 console.log('⏱️ Candle generator running...');
+
+const { updateCategories } = require('./categoryUpdater');
+
+setInterval(() => {
+  updateCategories();
+}, 5 * 60 * 1000); // every 5 minutes
