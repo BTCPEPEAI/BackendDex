5c650d2445ed4d8cb2ba475f69efd6b0c32a5565
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-29 21:22:07 +0530

    Update Coin.js

--- models/Coin.js
+++ models/Coin.js
@@ -7,6 +7,12 @@ const coinSchema = new mongoose.Schema({
   category: String, // trending, gainer, new, etc.
   price: Number,
   marketCap: Number,
+  isGainer: { type: Boolean, default: false },
+isTrending: { type: Boolean, default: false },
+previousPrice: Number,
+priceChange24h: Number,
+volumeUSD: Number,
+trusted: { type: Boolean, default: false },
   volume24h: Number,
   auditStatus: String,
   network: String,
