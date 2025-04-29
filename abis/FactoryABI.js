a771e1d82462fe543bba1f329757df92cb0fedb3
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-29 22:04:10 +0530

    Update FactoryABI.js

--- abis/FactoryABI.js
+++ abis/FactoryABI.js
@@ -1,17 +1,19 @@
 module.exports = [
-    {
-      "anonymous": false,
-      "inputs": [
-        { "indexed": true, "internalType": "address", "name": "token0", "type": "address" },
-        { "indexed": true, "internalType": "address", "name": "token1", "type": "address" },
-        { "indexed": false, "internalType": "address", "name": "pair", "type": "address" },
-        { "indexed": false, "internalType": "uint256", "name": "", "type": "uint256" }
-      ],
-      "name": "PairCreated",
-      "type": "event"
-    }
-  ];
-  
+  {
+    "name": "getPair",
+    "type": "function",
+    "inputs": [
+      { "name": "tokenA", "type": "address" },
+      { "name": "tokenB", "type": "address" }
+    ],
+    "outputs": [
+      { "name": "pair", "type": "address" }
+    ],
+    "stateMutability": "view"
+  }
+  // You can include other events like PairCreated if needed
+];
+
   module.exports = [
     "event PairCreated(address indexed token0, address indexed token1, address pair, uint)"
   ];
