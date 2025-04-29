7727d99fe2854166fd8d5e69cffc368c17dd6b47
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-29 16:39:36 +0530

    Update PairABI.js

--- abis/PairABI.js
+++ abis/PairABI.js
@@ -1,21 +1,6 @@
+// abis/PairABI.js
+
 module.exports = [
-    {
-      "anonymous": false,
-      "inputs": [
-        { "indexed": true, "internalType": "address", "name": "sender", "type": "address" },
-        { "indexed": false, "internalType": "uint256", "name": "amount0In", "type": "uint256" },
-        { "indexed": false, "internalType": "uint256", "name": "amount1In", "type": "uint256" },
-        { "indexed": false, "internalType": "uint256", "name": "amount0Out", "type": "uint256" },
-        { "indexed": false, "internalType": "uint256", "name": "amount1Out", "type": "uint256" },
-        { "indexed": true, "internalType": "address", "name": "to", "type": "address" }
-      ],
-      "name": "Swap",
-      "type": "event"
-    }
-  ];
-  
-  module.exports = [
-    "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)",
-    "function getReserves() view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"
-  ];
-  
+  "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)",
+  "function getReserves() view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"
+];
