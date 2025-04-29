f1be1f54dffefacd4594aed39a16aae1bc913f31
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-30 02:45:19 +0530

    Update FactoryABI.js

--- abis/FactoryABI.js
+++ abis/FactoryABI.js
@@ -1,5 +1,10 @@
-// abis/FactoryABI.js
 module.exports = [
+  {
+    "anonymous": false,
+    "inputs": [...],
+    "name": "PairCreated",
+    "type": "event"
+  },
   {
     "inputs": [
       { "internalType": "address", "name": "tokenA", "type": "address" },
@@ -9,16 +14,5 @@ module.exports = [
     "outputs": [{ "internalType": "address", "name": "pair", "type": "address" }],
     "stateMutability": "view",
     "type": "function"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      { "indexed": true, "internalType": "address", "name": "token0", "type": "address" },
-      { "indexed": true, "internalType": "address", "name": "token1", "type": "address" },
-      { "indexed": false, "internalType": "address", "name": "pair", "type": "address" },
-      { "indexed": false, "internalType": "uint256", "name": "", "type": "uint256" }
-    ],
-    "name": "PairCreated",
-    "type": "event"
   }
 ];
