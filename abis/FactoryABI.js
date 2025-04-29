1d9897b639c862722ae467008dad1b92c956d551
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-29 23:09:45 +0530

    Update FactoryABI.js

--- abis/FactoryABI.js
+++ abis/FactoryABI.js
@@ -1,20 +1,116 @@
-module.exports = [
-  {
-    "name": "getPair",
-    "type": "function",
-    "inputs": [
-      { "name": "tokenA", "type": "address" },
-      { "name": "tokenB", "type": "address" }
-    ],
-    "outputs": [
-      { "name": "pair", "type": "address" }
-    ],
-    "stateMutability": "view"
-  }
-  // You can include other events like PairCreated if needed
-];
-
-  module.exports = [
-    "event PairCreated(address indexed token0, address indexed token1, address pair, uint)"
-  ];
-  
+module.exports = {
+  PancakeSwapV2: [
+    {
+      "inputs": [
+        { "internalType": "address", "name": "_feeToSetter", "type": "address" }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        { "indexed": true, "internalType": "address", "name": "token0", "type": "address" },
+        { "indexed": true, "internalType": "address", "name": "token1", "type": "address" },
+        { "indexed": false, "internalType": "address", "name": "pair", "type": "address" },
+        { "indexed": false, "internalType": "uint256", "name": "", "type": "uint256" }
+      ],
+      "name": "PairCreated",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "INIT_CODE_PAIR_HASH",
+      "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ],
+      "name": "allPairs",
+      "outputs": [ { "internalType": "address", "name": "", "type": "address" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "allPairsLength",
+      "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        { "internalType": "address", "name": "tokenA", "type": "address" },
+        { "internalType": "address", "name": "tokenB", "type": "address" }
+      ],
+      "name": "createPair",
+      "outputs": [ { "internalType": "address", "name": "pair", "type": "address" } ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "feeTo",
+      "outputs": [ { "internalType": "address", "name": "", "type": "address" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "feeToSetter",
+      "outputs": [ { "internalType": "address", "name": "", "type": "address" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        { "internalType": "address", "name": "", "type": "address" },
+        { "internalType": "address", "name": "", "type": "address" }
+      ],
+      "name": "getPair",
+      "outputs": [ { "internalType": "address", "name": "", "type": "address" } ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [ { "internalType": "address", "name": "_feeTo", "type": "address" } ],
+      "name": "setFeeTo",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable"
+    },
+    {
+      "constant": false,
+      "inputs": [ { "internalType": "address", "name": "_feeToSetter", "type": "address" } ],
+      "name": "setFeeToSetter",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable"
+    }
+  ],
+  UniswapV2: [
+    // Same ABI structure as PancakeSwapV2
+    // Use the provided Uniswap V2 ABI here
+  ],
+  QuickSwap: [
+    // Same ABI structure as PancakeSwapV2
+    // Use the provided QuickSwap ABI here
+  ]
+};
