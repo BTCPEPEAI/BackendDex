fb988fb61284a49f5214be882567b83394d47419
BTCPEPE AI <134859277+BTCPEPEAI@users.noreply.github.com>
2025-04-30 20:55:13 +0530

    Update Trade.js

--- models/Trade.js
+++ models/Trade.js
@@ -6,9 +6,15 @@ const tradeSchema = new mongoose.Schema({
   tokenOut: String,
   amountIn: Number,
   amountOut: Number,
+  from: String,
+  to: String,
+  value: String,
+  network: String,
+  timestamp: Number,
   pairAddress: String,
   txHash: { type: String, unique: true },
   timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Trade', tradeSchema);
+
